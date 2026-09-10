2e12808353e15f2989ba2c5afd9a64d53d61563d
washington.silva <washingtoncqc@gmail.com>
2026-09-10 12:05:49 -0300

    adicionando configuração para gerar video no relatorio

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,6 +31,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
     baseURL: "https://loginxp.vercel.app",
+    video:'on'
   },
 
   /* Configure projects for major browsers */
